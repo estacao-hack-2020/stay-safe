affeaf8f8c7043b4099ed4be6dbdbb078d4ed31c
GuilhermeSant <guilherme_vinicius14@live.com>
2020-12-03 16:43:11 -0300

    JS - correção na checkagem de campos do formulario, adicionando Alert se todos estão preenchidos

--- js/validaForm.js
+++ js/validaForm.js
@@ -5,40 +5,41 @@ let formulario = document.getElementById("formHelpMe");
  let checked = false;
  let sendButton = document.querySelector(".btn-success")
 
+function infoSend(){
+    alert("informações enviados");
+    $('#modal').modal('toggle');
+}
+
 sendButton.addEventListener("click", function(event) {
     let inputs = document.querySelectorAll("#formHelpMe input");
     let textArea = document.querySelector("#formHelpMe textarea")
- 
-    for (i = 0; i < inputs.length; i++) {
-        if(checked == false){
+    if (checked == false) {
+    for (i = 0; i < inputs.length; i++) {         
 
-        
-        if(inputs[i].type == "password") {
-
-            if(document.getElementById(inputs[i].id).value.length < 11) {
-                document.getElementById(inputs[i].id+'Help').innerText = "Desculpe, você deve possuir uma senha com no mínimo 10 caracteres";
-            } else {
-                document.getElementById(inputs[i].id+'Help').innerText = '';
-            }
-        } else {
             if(document.getElementById(inputs[i].id).value.length < 1) {
                 document.getElementById(inputs[i].id+'Help').innerText = "Desculpe, este campo não pode ser deixado em branco";
                 document.getElementById(inputs[i].id+'Help').classList.remove('text-muted');
                 document.getElementById(inputs[i].id+'Help').style.color = "red";
-                
+                checked = false;
 
             } else {
                 document.getElementById(inputs[i].id+'Help').innerText = '';
-            }
-        }
+                checked = true;
+            }      
         
             if(document.getElementById("f_pedido").value.length < 1){
                 document.getElementById("f_pedido"+'Help').innerText = "Desculpe, este campo não pode ser deixado em branco";
                 document.getElementById("f_pedido"+'Help').classList.remove('text-muted');
                 document.getElementById("f_pedido"+'Help').style.color = "red";
+                checked = false;
+
             }else{
                 document.getElementById("f_pedido"+'Help').innerText = '';
+                checked = true;
             }
-    }
-    }
+         }
+    } else{
+        infoSend();
+        checked = false;
+        }
 });
