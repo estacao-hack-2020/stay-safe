37bd49b72023003c5228a73904565f385d5f7b05
GuilhermeSant <guilherme_vinicius14@live.com>
2020-12-10 09:36:04 -0300

    JS - Correção validação de Formulario, validando ao pressionar botão Enviar e a cada alteração dentro do Formulario

--- js/validaForm.js
+++ js/validaForm.js
@@ -3,43 +3,53 @@
 
 let formulario = document.getElementById("formHelpMe");
  let checked = false;
- let sendButton = document.querySelector(".btn-success")
+ let sendButton = document.querySelector(".btn-success");
+
+ let inputs = document.querySelectorAll("#formHelpMe input");
+ let textArea = document.querySelector("#formHelpMe textarea")
 
 function infoSend(){
     alert("informações enviados");
     $('#modal').modal('toggle');
 }
 
-sendButton.addEventListener("click", function(event) {
-    let inputs = document.querySelectorAll("#formHelpMe input");
-    let textArea = document.querySelector("#formHelpMe textarea")
-    if (checked == false) {
+function validaCamposForm(){
     for (i = 0; i < inputs.length; i++) {         
 
-            if(document.getElementById(inputs[i].id).value.length < 1) {
-                document.getElementById(inputs[i].id+'Help').innerText = "Desculpe, este campo não pode ser deixado em branco";
-                document.getElementById(inputs[i].id+'Help').classList.remove('text-muted');
-                document.getElementById(inputs[i].id+'Help').style.color = "red";
-                checked = false;
+        if(document.getElementById(inputs[i].id).value.length < 1) {
+            document.getElementById(inputs[i].id+'Help').innerText = "Desculpe, este campo não pode ser deixado em branco";
+            document.getElementById(inputs[i].id+'Help').classList.remove('text-muted');
+            document.getElementById(inputs[i].id+'Help').style.color = "red";
+            checked = false;
 
-            } else {
-                document.getElementById(inputs[i].id+'Help').innerText = '';
-                checked = true;
-            }      
-        
-            if(document.getElementById("f_pedido").value.length < 1){
-                document.getElementById("f_pedido"+'Help').innerText = "Desculpe, este campo não pode ser deixado em branco";
-                document.getElementById("f_pedido"+'Help').classList.remove('text-muted');
-                document.getElementById("f_pedido"+'Help').style.color = "red";
-                checked = false;
+        } else {
+            document.getElementById(inputs[i].id+'Help').innerText = '';
+            checked = true;
+        }      
+    
+        if(document.getElementById("f_pedido").value.length < 1){
+            document.getElementById("f_pedido"+'Help').innerText = "Desculpe, este campo não pode ser deixado em branco";
+            document.getElementById("f_pedido"+'Help').classList.remove('text-muted');
+            document.getElementById("f_pedido"+'Help').style.color = "red";
+            checked = false;
 
-            }else{
-                document.getElementById("f_pedido"+'Help').innerText = '';
-                checked = true;
-            }
-         }
+        }else{
+            document.getElementById("f_pedido"+'Help').innerText = '';
+            checked = true;
+        }
+     }
+}
+
+formulario.onchange = function(){
+    validaCamposForm()
+}
+
+sendButton.addEventListener("click", function(event) {
+    if (checked == false) {
+        validaCamposForm()
+
     } else{
         infoSend();
-        checked = false;
         }
 });
+
