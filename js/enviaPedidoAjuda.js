999b51bf0bd6a666f4deb63c1a59e6995f9de690
Lucas Souza <lucasouliveira@gmail.com>
2020-12-10 21:47:42 +0000

    função envia pedido de ajuda

---
+++ js/enviaPedidoAjuda.js
@@ -0,0 +1,36 @@
+const getInputValue = selector => document.querySelector(selector).value;
+
+export default function enviaAjuda() {
+
+
+    // TODO: pegar latitude e longitude atuais
+    const latitude = 0, longitude = 0;
+
+    const inputValues = {
+        nome: getInputValue('#f_nome'),
+        idade: getInputValue('#f_idade'),
+        mensagem: getInputValue('#f_pedido'),
+        telefone: getInputValue('#f_telefone'),
+        email: getInputValue('#f_email'),
+        latitude, longitude,
+    }
+
+    fetch('https://staysafe-api.herokuapp.com/help', {
+
+        method: 'POST',
+        body: inputValues
+
+    }).then(response => {
+
+        console.log(response);
+        alert('informações enviados');
+        $('#modalHelpMe').modal('toggle');
+
+    }).catch(err => {
+
+        alert('houver um erro no envio de ajuda');
+        console.error(err);
+
+    });
+
+}
--- js/validaForm.js
+++ js/validaForm.js
@@ -1,55 +1,51 @@
 // Validação formulario
-
+import infoSend from './enviaPedidoAjuda.js';
 
 let formulario = document.getElementById("formHelpMe");
- let checked = false;
- let sendButton = document.querySelector(".btn-success");
+let checked = false;
+let sendButton = document.querySelector(".btn-success");
 
- let inputs = document.querySelectorAll("#formHelpMe input");
- let textArea = document.querySelector("#formHelpMe textarea")
+let inputs = document.querySelectorAll("#formHelpMe input");
+let textArea = document.querySelector("#formHelpMe textarea")
 
-function infoSend(){
-    alert("informações enviados");
-    $('#modal').modal('toggle');
-}
+function validaCamposForm() {
+    for (i = 0; i < inputs.length; i++) {
 
-function validaCamposForm(){
-    for (i = 0; i < inputs.length; i++) {         
-
-        if(document.getElementById(inputs[i].id).value.length < 1) {
-            document.getElementById(inputs[i].id+'Help').innerText = "Desculpe, este campo não pode ser deixado em branco";
-            document.getElementById(inputs[i].id+'Help').classList.remove('text-muted');
-            document.getElementById(inputs[i].id+'Help').style.color = "red";
+        if (document.getElementById(inputs[i].id).value.length < 1) {
+            document.getElementById(inputs[i].id + 'Help').innerText = "Desculpe, este campo não pode ser deixado em branco";
+            document.getElementById(inputs[i].id + 'Help').classList.remove('text-muted');
+            document.getElementById(inputs[i].id + 'Help').style.color = "red";
             checked = false;
 
         } else {
-            document.getElementById(inputs[i].id+'Help').innerText = '';
-            checked = true;
-        }      
-    
-        if(document.getElementById("f_pedido").value.length < 1){
-            document.getElementById("f_pedido"+'Help').innerText = "Desculpe, este campo não pode ser deixado em branco";
-            document.getElementById("f_pedido"+'Help').classList.remove('text-muted');
-            document.getElementById("f_pedido"+'Help').style.color = "red";
-            checked = false;
-
-        }else{
-            document.getElementById("f_pedido"+'Help').innerText = '';
+            document.getElementById(inputs[i].id + 'Help').innerText = '';
             checked = true;
         }
-     }
+
+        if (document.getElementById("f_pedido").value.length < 1) {
+            document.getElementById("f_pedido" + 'Help').innerText = "Desculpe, este campo não pode ser deixado em branco";
+            document.getElementById("f_pedido" + 'Help').classList.remove('text-muted');
+            document.getElementById("f_pedido" + 'Help').style.color = "red";
+            checked = false;
+
+        } else {
+            document.getElementById("f_pedido" + 'Help').innerText = '';
+            checked = true;
+        }
+    }
 }
 
-formulario.onchange = function(){
+formulario.onchange = function () {
     validaCamposForm()
 }
 
-sendButton.addEventListener("click", function(event) {
+sendButton.addEventListener("click", function (event) {
+    event.preventDefault();
+
     if (checked == false) {
         validaCamposForm()
 
-    } else{
+    } else {
         infoSend();
-        }
+    }
 });
-
