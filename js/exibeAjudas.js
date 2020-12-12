c0232bb918b549aea3f57a6c2ebaca0cb29c1e7a
lucasliet <lucasouliveira@gmail.com>
2020-12-11 21:35:07 -0300

    fix criação de div

--- js/exibeAjudas.js
+++ js/exibeAjudas.js
@@ -1,34 +1,32 @@
 import { getAjudas } from './HelpApi.js';
 
-window.onload = () => {
-  getAjudas().then( ajudas => {
-    const row = document.querySelector('#help div[class="row"]');
+getAjudas().then(ajudas => {
+  const row = document.querySelector('#help div[class="row"]');
 
-    console.info(ajudas)
-    
-    ajudas
-      .filter( ajuda => ajuda.status === 'aguardando' )
-      .map( ajuda => {
-      
-        const ajudaElement = document.createElement('div');
-      
-        ajudaElement.innerHTML = `
-          <div class="col-xs-12 col-md-4 ">
-            <div class="boxHelp">
-              <h3>Cidade</h3>
-              <ul>
-                <li name="pessoa">${ajuda.nome}</li>
-                <li name="idade">${ajuda.idade} anos</li>
-                <li name="pedido" class="pedido">${ajuda.mensagem}<li>
-                <li name="email">${ajuda.email}</li>
-              </ul>
-            </div>
-          </div>
-        `;
+  ajudas
+    .filter(ajuda => ajuda.status === 'aguardando')
+    .map(ajuda => {
 
-        row.appendChild(ajudaElement);
-      
-      });
+      const ajudaElement = document.createElement('div');
+      ajudaElement.classList.add('col-xs-12', 'col-md-4');
 
-  });
-}
+      // TODO: conseguir cidade a partir da latitude e longitude dentro de ajuda
+      const cidade = 'Cidade Teste'
+
+      ajudaElement.innerHTML = `
+        <div class="boxHelp">
+          <h3>${cidade}</h3>
+          <ul>
+            <li name="pessoa">${ajuda.nome}</li>
+            <li name="idade">${ajuda.idade} anos</li>
+            <li name="email"><small>email:</small> ${ajuda.email}</li>
+            <li name="pedido" class="pedido">${ajuda.mensagem}<li>
+          </ul>
+        </div>
+      `;
+
+      row.appendChild(ajudaElement);
+
+    });
+
+});
