22bec7c9aa6df000b9232c5a6bd4518106f8ce2b
willcrdo <74738227+willcrdo@users.noreply.github.com>
2020-12-13 17:24:47 -0300

    Update renderMap.js

--- js/renderMap.js
+++ js/renderMap.js
@@ -2,8 +2,7 @@ import { getAjudas } from './HelpApi.js';
 export let lat, lon;
 
 
-//var mapa=new google.maps.Map(document.getElementById("map-canvas"));
-var mapa = new google.maps.Map();
+var mapa = new google.maps.Map(document.getElementById("map-canvas"));
 var infoWindow = new google.maps.InfoWindow();
 var marker = new google.maps.Marker();
 
