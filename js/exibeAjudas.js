afa2f5b3f396c8560e7be0dff57976e8c3517fd0
lucasliet <lucasouliveira@gmail.com>
2020-12-12 23:19:21 -0300

    cidade de cada pedido de ajuda pela geolocation

--- js/exibeAjudas.js
+++ js/exibeAjudas.js
@@ -1,31 +1,36 @@
 import { getAjudas } from './HelpApi.js';
+import { getLocationData } from './geolocation.js'
 
-getAjudas().then(ajudas => {
+getAjudas().then( ajudas => {
   const row = document.querySelector('#help div[class="row"]');
 
   ajudas
-    .filter(ajuda => ajuda.status === 'aguardando')
-    .map(ajuda => {
+    .filter( ajuda => ajuda.status === 'aguardando' )
+    .map( ajuda => {
+      const { latitude, longitude } = ajuda;
 
-      const ajudaElement = document.createElement('div');
-      ajudaElement.classList.add('col-xs-12', 'col-md-4');
+      getLocationData(latitude, longitude).then( geoData => {
 
-      // TODO: conseguir cidade a partir da latitude e longitude dentro de ajuda
-      const cidade = 'Cidade Teste'
+        const ajudaElement = document.createElement('div');
+        ajudaElement.classList.add('col-xs-12', 'col-md-4');
 
-      ajudaElement.innerHTML = `
-        <div class="boxHelp">
-          <h3>${cidade}</h3>
-          <ul>
-            <li name="pessoa">${ajuda.nome}</li>
-            <li name="idade">${ajuda.idade} anos</li>
-            <li name="email"><small>email:</small> ${ajuda.email}</li>
-            <li name="pedido" class="pedido">${ajuda.mensagem}<li>
-          </ul>
-        </div>
-      `;
+        const cidade = geoData.city;
 
-      row.appendChild(ajudaElement);
+        ajudaElement.innerHTML = `
+            <div class="boxHelp">
+              <h3>${cidade}</h3>
+              <ul>
+                <li name="pessoa">${ajuda.nome}</li>
+                <li name="idade">${ajuda.idade} anos</li>
+                <li name="email"><small>email:</small> ${ajuda.email}</li>
+                <li name="pedido" class="pedido">${ajuda.mensagem}<li>
+              </ul>
+            </div>
+          `;
+
+        row.appendChild(ajudaElement);
+
+      });
 
     });
 
